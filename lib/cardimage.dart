c7998cd8c9018d676d06c9c65b5d29eb151769c6
Carlos H (windows) <carlos.hernandezmar@anahuac.mx>
2021-04-17 01:49:50 -0500

    se agrego botones de navegacion y favoritos

--- lib/cardimage.dart
+++ lib/cardimage.dart
@@ -1,37 +1,51 @@
 import 'package:flutter/material.dart';
+import 'floating_action_button_green.dart';
 
-class CardImage extends StatelessWidget {
+class  CardImage extends StatelessWidget {
 
-  String pathImage = "asset/people,jpg";
+  String pathImage = "assets/img/beach.jpeg";
+
   CardImage(this.pathImage);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
     final card = Container(
-      height: 200.0,
-      width: 270.0,
+      height: 350.0,
+      width: 250.0,
       margin: EdgeInsets.only(
-        top: 80.0,
-        left: 80.0
+          top: 50.0,
+          left: 60.0,
+          bottom: 20.0
+
       ),
+
       decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage(pathImage)
-        ),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black45,
-          blurRadius: 15.0,
-          offset: Offset(30.0, 30.0)
-        )
-      ]
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(pathImage)
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          shape: BoxShape.rectangle,
+          boxShadow: <BoxShadow>[
+            BoxShadow (
+                color:  Colors.black45,
+                blurRadius: 10.0,
+                offset: Offset(4.0, 7.0)
+            )
+          ]
+
       ),
     );
 
-
-    return card;
+    return Stack(
+      alignment: Alignment(0.9,0.9),
+      children: <Widget>[
+        card,
+        FloatingActionButtonGreen()
+      ],
+    );
   }
 
 }
---
+++ lib/test_statefulwidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class testclass extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
